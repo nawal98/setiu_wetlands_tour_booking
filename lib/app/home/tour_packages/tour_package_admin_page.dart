558484598318ad9705e203101c1d6c750e814d9f
nawal98 <59013118+nawal98@users.noreply.github.com>
2020-04-05 23:52:43 +0800

    tour package

--- lib/app/home/tour_packages/tour_package_admin_page.dart
+++ lib/app/home/tour_packages/tour_package_admin_page.dart
@@ -15,48 +15,23 @@ import 'package:setiuwetlandstourbooking/common_widget/platform_alert_dialog.dar
 class TourPackageAdmin extends StatelessWidget {
 //  List<String> events = ["Tour Package", "Tour Activity", "Resort", "Staff"];
 
-  Future<void> _signOut(BuildContext context) async {
-    try {
-      final auth = Provider.of<AuthBase>(context);
-      await auth.signOut();
-    } catch (e) {
-      print(e.toString());
-    }
-  }
 
-  Future<void> _confirmSignOut(BuildContext context) async {
-    final didRequestSignOut = await PlatformAlertDialog(
-      title: 'Logout',
-      content: 'Are you sure that you want to logout?',
-      cancelActionText: 'Cancel',
-      defaultActionText: 'Logout',
-    ).show(context);
-    if (didRequestSignOut == true) {
-      _signOut(context);
-    }
-  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("List of Tour Package"),
         actions: <Widget>[
-          FlatButton(
-            child: Text(
-              'Logout',
-              style: TextStyle(
-                fontSize: 18.0,
-              ),
+          IconButton(
+            icon: Icon(Icons.add, color: Colors.black),
+            onPressed: () => EditTourPackagePage.show(
+              context, database: Provider.of<Database>(context),
             ),
-            onPressed: () => _confirmSignOut(context),
           ),
         ],
       ),
       body: _buildContents(context),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => EditTourPackagePage.show(context),
-      ),
     );
   }
 
@@ -74,7 +49,7 @@ class TourPackageAdmin extends StatelessWidget {
             ),
             direction: DismissDirection.endToStart,
             confirmDismiss: (DismissDirection direction) async {
-             return await showDialog(
+              return await showDialog(
                 context: context,
                 builder: (BuildContext context) {
                   return AlertDialog(
